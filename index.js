8464fcfd7743cbf833a7a83dd5e17386f2b49533
heb1k0 <alejandrozuriguel@gmail.com>
2021-05-22 23:01:40 +0200

    fageda

--- index.js
+++ index.js
@@ -71,12 +71,24 @@ client.on('message', msg => {
 });
 
 client.on('message_create', msj =>{
+
+    var mensaje = msj.body.toLowerCase();
     
-    if(msj.body.includes("!email")){
+    if(mensaje.includes("!email")){
         var result = getEmail(msj.body);
         console.log(result)
         msj.reply("Email enviado a "+result);
     }
+
+    if(mensaje.includes("fageda")){
+
+        var hoy = new Date();
+        var fecha = hoy.getFullYear()  + '-' + ( hoy.getMonth() + 1 ) + '-' + hoy.getDate();
+        var res = daysBetween(fecha,"2021-05-29")
+        msj.reply("Solo quedan "+res+" dias mariquitas!");
+        
+    }
+
 })
 
 getEmail = (email) =>{
@@ -86,6 +98,32 @@ getEmail = (email) =>{
     return match[1];
 
 }
+
+
+
+function daysBetween(date1, date2){ 
+    if (date1.indexOf("-") != -1) { date1 = date1.split("-"); } else if (date1.indexOf("/") != -1) { date1 = date1.split("/"); } else { return 0; } 
+    if (date2.indexOf("-") != -1) { date2 = date2.split("-"); } else if (date2.indexOf("/") != -1) { date2 = date2.split("/"); } else { return 0; } 
+    if (parseInt(date1[0], 10) >= 1000) { 
+        var sDate = new Date(date1[0]+"/"+date1[1]+"/"+date1[2]);
+    } else if (parseInt(date1[2], 10) >= 1000) { 
+        var sDate = new Date(date1[2]+"/"+date1[0]+"/"+date1[1]);
+    } else { 
+        return 0; 
+    } 
+    if (parseInt(date2[0], 10) >= 1000) { 
+        var eDate = new Date(date2[0]+"/"+date2[1]+"/"+date2[2]);
+    } else if (parseInt(date2[2], 10) >= 1000) { 
+        var eDate = new Date(date2[2]+"/"+date2[0]+"/"+date2[1]);
+    } else { 
+        return 0; 
+    } 
+    var one_day = 1000*60*60*24; 
+    var daysApart = Math.abs(Math.ceil((sDate.getTime()-eDate.getTime())/one_day)); 
+
+    return daysApart; 
+ } 
+
 
 
 
